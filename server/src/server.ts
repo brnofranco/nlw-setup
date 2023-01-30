68059b3ea78c2ac89903656c03ad51c6597fa31f
Bruno Franco <brunofrancodemoraes@gmail.com>
2023-01-30 08:49:18 -0300

    aula 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,15 @@
-import Fastify from 'fastify';
-import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
-app.register(cors, {
-	origin: ['http://localhost:3000'],
-});
-
-app.get('/', async () => {
-	const habits = await prisma.habit.findMany();
-
-	return habits;
-});
+app.register(cors);
+app.register(appRoutes);
 
 app.listen({
 	port: 3333,
 }).then(() => {
-	console.log('HTTP Server Running');
+	console.log("HTTP Server Running");
 });
